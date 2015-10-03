b17dfb5473db57a02b4d36d71641471df14672c8
Jean Parpaillon <jean.parpaillon@free.fr>
2015-10-03 11:10:41 +0200

    Fix indent

--- src/erocci_backend_dbus.erl
+++ src/erocci_backend_dbus.erl
@@ -31,105 +31,105 @@
 
 %% occi_backend callbacks
 -export([init/1,
-		 terminate/1]).
+	 terminate/1]).
 -export([update/2,
-		 save/2,
-		 delete/2,
-		 find/2,
-		 load/3,
-		 action/3]).
+	 save/2,
+	 delete/2,
+	 find/2,
+	 load/3,
+	 action/3]).
 
 -record(state, {conn      :: dbus_connection(),
-				backend   :: dbus_proxy()}).
+		backend   :: dbus_proxy()}).
 
 %%%===================================================================
 %%% occi_backend callbacks
 %%%===================================================================
 init(#occi_backend{opts=Props}) ->
-	try parse_opts(Props) of
-		{Service, Opts} ->
-			case connect_backend(Service) of
-				{ok, Bus, Backend} ->
-					?info("Initializing backend service: ~s~n", [Service]),
-					case dbus_proxy:call(Backend, ?BACKEND_IFACE, <<"init">>, [Opts]) of
-						{ok, Schemas} ->
-							{ok, process_schemas(Schemas, []), #state{conn=Bus, backend=Backend}};
-						{error, {Code, Err}} ->
-							?debug("Error initializing: ~n"
-								   "Code=~s~n"
-								   "Reason=~n~s~n", [Code, Err]),
-							{error, Err}
-					end;
-				{error, Err} ->
-					{error, Err}
-			end
+    try parse_opts(Props) of
+	{Service, Opts} ->
+	    case connect_backend(Service) of
+		{ok, Bus, Backend} ->
+		    ?info("Initializing backend service: ~s~n", [Service]),
+		    case dbus_proxy:call(Backend, ?BACKEND_IFACE, <<"init">>, [Opts]) of
+			{ok, Schemas} ->
+			    {ok, process_schemas(Schemas, []), #state{conn=Bus, backend=Backend}};
+			{error, {Code, Err}} ->
+			    ?debug("Error initializing: ~n"
+				   "Code=~s~n"
+				   "Reason=~n~s~n", [Code, Err]),
+			    {error, Err}
+		    end;
+		{error, Err} ->
+		    {error, Err}
+	    end
     catch throw:Err -> {error, Err}
     end.
 
 
 terminate(#state{backend=Backend}) ->
     case dbus_proxy:call(Backend, ?BACKEND_IFACE, <<"terminate">>, []) of
-		_ -> ok
+	_ -> ok
     end.
 
 save(#state{backend=Backend}=State, #occi_node{}=Node) ->
     ?info("[~p] save(~p)~n", [?MODULE, Node]),
     case dbus_proxy:call(Backend, ?BACKEND_IFACE, <<"save">>, [occi_renderer_dbus:render(Node)]) of
-		ok ->
-			{ok, State};
-		{error, Err} ->
-			{{error, Err}, State}
+	ok ->
+	    {ok, State};
+	{error, Err} ->
+	    {{error, Err}, State}
     end.
 
 delete(#state{backend=Backend}=State, #occi_node{id=Uri}=Node) ->
     ?info("[~p] delete(~p)~n", [?MODULE, Node]),
     case dbus_proxy:call(Backend, ?BACKEND_IFACE, <<"delete">>, [occi_uri:to_binary(Uri)]) of
-		ok ->
-			{ok, State};
-		{error, Err} ->
-			{{error, Err}, State}
+	ok ->
+	    {ok, State};
+	{error, Err} ->
+	    {{error, Err}, State}
     end.
 
 
 update(#state{backend=Backend}=State, #occi_node{}=Node) ->
     ?info("[~p] update(~p)~n", [?MODULE, Node]),
     case dbus_proxy:call(Backend, ?BACKEND_IFACE, <<"update">>, [occi_renderer_dbus:render(Node)]) of
-		ok ->
-			{ok, State};
-		{error, Err} ->
-			{{error, Err}, State}
+	ok ->
+	    {ok, State};
+	{error, Err} ->
+	    {{error, Err}, State}
     end.
 
 
 find(#state{backend=Backend}=State, #occi_node{id=Uri}=_N) ->
     ?info("[~p] find(~p)~n", [?MODULE, _N]),
     case dbus_proxy:call(Backend, ?BACKEND_IFACE, <<"find">>, [occi_uri:to_binary(Uri)]) of
-		{ok, [Node]} ->
-			{{ok, [occi_parser_dbus:parse(Node)]}, State};
-		{error, Err} ->
-			{{error, Err}, State}
+	{ok, [Node]} ->
+	    {{ok, [occi_parser_dbus:parse(Node)]}, State};
+	{error, Err} ->
+	    {{error, Err}, State}
     end.
 
 
 load(#state{backend=Backend}=State, #occi_node{id=Uri}=Node, _Opts) ->
     ?info("[~p] load(~p)~n", [?MODULE, Uri]),
     case dbus_proxy:call(Backend, ?BACKEND_IFACE, <<"load">>, [occi_renderer_dbus:render(Node)]) of
-		{ok, N} ->
-			{{ok, occi_parser_dbus:parse(N)}, State};
-		{error, Err} ->
-			{{error, Err}, State}
+	{ok, N} ->
+	    {{ok, occi_parser_dbus:parse(N)}, State};
+	{error, Err} ->
+	    {{error, Err}, State}
     end.
 
 
 action(#state{backend=Backend}=State, #uri{}=Id, #occi_action{}=A) ->
     ?info("[~p] action(~p, ~p)~n", [?MODULE, Id, A]),
     Args = [occi_renderer_dbus:render(Id), 
-			occi_renderer_dbus:render(A)],
+	    occi_renderer_dbus:render(A)],
     case dbus_proxy:call(Backend, ?BACKEND_IFACE, <<"load">>, Args) of
-		ok ->
-			{ok, State};
-		{error, Err} ->
-			{{error, Err}, State}
+	ok ->
+	    {ok, State};
+	{error, Err} ->
+	    {{error, Err}, State}
     end.
 
 %%%===================================================================
@@ -137,24 +137,24 @@ action(#state{backend=Backend}=State, #uri{}=Id, #occi_action{}=A) ->
 %%%===================================================================
 parse_opts(Props) ->
     Srv = case proplists:get_value(service, Props) of
-			  undefined -> throw({error, {missing_opt, service}});
-			  Str -> list_to_binary(Str)
-		  end,
-	Opts = case proplists:get_value(opts, Props) of
-			   undefined -> [];
-			   V -> V
-		   end,
-	{Srv, Opts}.
-    
+	      undefined -> throw({error, {missing_opt, service}});
+	      Str -> list_to_binary(Str)
+	  end,
+    Opts = case proplists:get_value(opts, Props) of
+	       undefined -> [];
+	       V -> V
+	   end,
+    {Srv, Opts}.
+
 
 connect_backend(Service) ->
     case dbus_bus_connection:connect(session) of
-		{ok, Bus} ->
-			case dbus_proxy:start_link(Bus, Service) of
-				{ok, Backend} -> {ok, Bus, Backend};
-				{error, _} = Err -> Err
-			end;
+	{ok, Bus} ->
+	    case dbus_proxy:start_link(Bus, Service) of
+		{ok, Backend} -> {ok, Bus, Backend};
 		{error, _} = Err -> Err
+	    end;
+	{error, _} = Err -> Err
     end.
 
 
